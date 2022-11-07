90ae718b29d425534757d3e713cf67adb8c757ef
Dzikri <dzikrihak@gmail.com>
2022-11-07 07:21:50 +0700

    add elif seleksi

---
+++ seleksi/elif01.py
@@ -0,0 +1,17 @@
+today = input("Hari apa sekarang ?  ")
+
+if(today=="Senin"):
+    print("I love Monday")
+
+elif(today=="Selasa"):
+    print("Masih hari selasa, hari kerja / sekolah")
+elif(today=="Rabu"):
+    print("Tenang masih hari rabu, semangat ya!!")
+elif(today=="Kamis"):
+    print("Alhamdulillah sudah hari kamis")
+elif(today=="Jumat"):
+    print("Jumat Berkah....")
+elif(today=="Sabtu"):
+    print("Olahraga dulu ya..")
+elif(today=="Minggu"):
+    print("Bolehlah jalan-jalan...")
---
+++ seleksi/elif02.py
@@ -0,0 +1,8 @@
+x = int(input("Masukan anggka ?  "))
+
+if x < 10:
+    print(f"{x} lebih kecil dari 10 ")
+elif x == 10:
+    print(f"{x} berarti sama dengan 10")
+else:
+    print(f"{x} bisa jadi lebih dari 10")
